598f0402863a57b61b87e9656ed161ac428664f6
Zhu Liang <60082890+zhuliangecon@users.noreply.github.com>
2024-09-30 15:18:40 -0400

    Update R Markdown document with fixed effect regression and additional regression analysis

--- example.R
+++ example.R
@@ -59,7 +59,50 @@ library(fixest)
 # price, horsepower, width, height, weight, fuel, demographic, ln_pop, ln_gdp are 
 # the independent variables
 
-fe_reg <- feols(y ~ price + horsepower + width + height + weight + fuel + demographic + ln_pop + ln_gdp | year + country + brand, data = data)
+fe_reg_a <- feols(y ~ price + horsepower + width + height + weight + fuel + demographic + ln_pop + ln_gdp | year + country + brand, data = data)
 
 # print the regression results
-summary(fe_reg)
+summary(fe_reg_a)
+
+# run a similar regression as above but with the hetergeneity price coefficient across countries (add a price*country interaction term)
+
+fe_reg_b <- feols(y ~ price + horsepower + width + height + weight + fuel + demographic + ln_pop + ln_gdp + price:country | year + country + brand, data = data)
+
+# print the regression results
+summary(fe_reg_b)
+
+# Test whether the price coefficients are the same across countries.
+# Test if price:countryFrance = price:countryGermany = price:countryItaly = price:countryUK = 0
+
+# install the `car` package for the linear hypothesis test
+if (!requireNamespace("car", quietly = TRUE)) {
+  install.packages("car")
+}
+
+library(car)
+
+# run the linear hypothesis test
+linearHypothesis(fe_reg_b, c("price:countryFrance = 0", "price:countryGermany = 0", "price:countryItaly = 0", "price:countryUK = 0"))
+
+# Construct the IV set 
+data_with_iv <- data %>%
+  group_by(country, year) %>%
+  mutate(IV_horsepower = horsepower - (sum(horsepower) - horsepower)/(n() - 1),
+         IV_width = width - (sum(width) - width)/(n() - 1),
+         IV_fuel = fuel - (sum(fuel) - fuel)/(n() - 1),
+         IV_height = height - (sum(height) - height)/(n() - 1),
+         IV_weight = weight - (sum(weight) - weight)/(n() - 1),
+         IV_demographic = demographic - (sum(demographic) - demographic)/(n() - 1),
+  )
+
+# summary statistics of the IV set (new variables)
+data_with_iv %>% 
+  select(IV_horsepower, IV_width, IV_fuel, IV_height, IV_weight, IV_demographic) %>%
+  summary()
+
+# run a fixed effect regression with the IV set (price is the endogenous,
+# price ~ IV_horsepower + IV_width + IV_fuel + IV_height + IV_weight + IV_demographic) by ivfeols
+iv_fe_reg_c <- feols(y ~ horsepower + width + height + weight + fuel + demographic + ln_pop + ln_gdp | year + country + brand | price ~ IV_horsepower + IV_width + IV_fuel + IV_height + IV_weight + IV_demographic, data = data_with_iv)
+
+# print the regression results
+summary(iv_fe_reg_c)
